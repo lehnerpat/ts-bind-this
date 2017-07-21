f8595e66018316d9eb7e450e48646058a29bf8f9
Patrick Lehner <nevik@Kaoru.local>
2017-07-21 21:37:55 +0200

    (feat) create problematic situation

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,48 @@ import 'milligram';
 
 import './styles.css';
 
-export class Main {
-    public constructor() {
-        console.log('making a main!');
+const makeHTML = (orderItem: OrderItem): HTMLElement => {
+    const makeDiv = (...classes: string[]) => {
+        const result = document.createElement('div');
+        result.classList.add(...classes);
+        return result;
+    };
+
+    const rowDiv = makeDiv('row');
+
+    const nameColDiv = makeDiv('column', 'column-50');
+    nameColDiv.appendChild(document.createTextNode(orderItem.name));
+    rowDiv.appendChild(nameColDiv);
+
+    const btnColDiv = makeDiv('column', 'column-50');
+    rowDiv.appendChild(btnColDiv);
+
+    const button = document.createElement('button');
+    button.classList.add('button');
+    button.appendChild(document.createTextNode('Order!'));
+    btnColDiv.appendChild(button);
+
+    return rowDiv;
+};
+
+class OrderItem {
+    public constructor(public readonly name: string) { }
+
+    public onOrder() {
+        alert(`You're ordering: ${this.name}!`);
     }
 }
 
-new Main();
+
+const listContainer = document.getElementById('list-container');
+
+['Pizza', 'Burgers', 'Pasta']
+    .map((name) => new OrderItem(name))
+    .forEach((orderItem) => { 
+        const orderItemDiv = makeHTML(orderItem);
+
+        // the problematic bit:
+        orderItemDiv.addEventListener('click', orderItem.onOrder);
+
+        listContainer.appendChild(orderItemDiv) 
+    });
