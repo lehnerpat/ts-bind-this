f9fb93dc8ca5ef2fd140d83216faf7c7aa668a9b
Patrick Lehner <nevik@Kaoru.local>
2017-07-21 20:31:50 +0200

    (feat) add webpack

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+
+export class Main {
+    public constructor() {
+        console.log('making a main!');
+    }
+}
+
+new Main();
---
+++ webpack.config.ts
@@ -0,0 +1,50 @@
+import * as webpack from 'webpack';
+import * as path from 'path';
+
+declare var __dirname;
+
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const outputFolder = path.resolve(__dirname, 'dist');
+
+const config: webpack.Configuration = {
+    entry: './src/main.ts',
+
+    /** https://webpack.js.org/configuration/devtool/ */
+    devtool: 'inline-source-map',
+
+    /** https://webpack.js.org/configuration/dev-server/ */
+    devServer: {
+        contentBase: outputFolder,
+        overlay: true
+    },
+
+    /** https://webpack.js.org/configuration/output/ */
+    output: {
+        path: outputFolder,
+        filename: '[name].[chunkhash].bundle.js'
+    },
+
+    resolve: {
+        /** https://webpack.js.org/configuration/resolve/#resolve-extensions */
+        extensions: ['.ts', '.tsx', '.js']
+    },
+
+    module: {
+        /** https://webpack.js.org/configuration/module/#module-rules */
+        rules: [
+            /** https://github.com/TypeStrong/ts-loader */
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    },
+
+    plugins: [
+        /** https://github.com/jantimon/html-webpack-plugin */
+        new HtmlWebpackPlugin({
+            title: 'TS ! bind ! this -- Example'
+        })
+    ]
+
+};
+
+export default config;
